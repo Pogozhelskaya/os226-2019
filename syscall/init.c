fb6df17b4c9a8dfc98570aeca3be054cad6d8ed3
Vlada Pogozhelskaya <31728695+Pogozhelskaya@users.noreply.github.com>
2019-09-15 10:01:11 +0300

    Update init.c

--- syscall/init.c
+++ syscall/init.c
@@ -18,31 +18,31 @@ void os_sighnd(int sig, siginfo_t *info, void *ctx) {
 	if (0x138b == *(uint16_t *) uc->uc_mcontext.gregs[REG_RIP]) {
 		uc->uc_mcontext.gregs[REG_RDX] = 100000 + counter;
 		uc->uc_mcontext.gregs[REG_RIP] += 2;
-	} else if  (0x0004538b == *(uint32_t *) uc->uc_mcontext.gregs[REG_RIP] && 0x00ffffff) {
+	} else if  (0x0004538b == *(uint32_t *) uc->uc_mcontext.gregs[REG_RIP] & 0x00ffffff) {
 		uc->uc_mcontext.gregs[REG_RDX] = 100000 + 1000 + counter;
 		uc->uc_mcontext.gregs[REG_RIP] += 3;
-	} else if  (0x0008538b == *(uint32_t *) uc->uc_mcontext.gregs[REG_RIP] && 0x00ffffff) {
+	} else if  (0x0008538b == *(uint32_t *) uc->uc_mcontext.gregs[REG_RIP] & 0x00ffffff) {
 		uc->uc_mcontext.gregs[REG_RDX] = 100000 + 2000 + counter;
 		uc->uc_mcontext.gregs[REG_RIP] += 3;
-	} else if  (0x000c538b == *(uint32_t *) uc->uc_mcontext.gregs[REG_RIP] && 0x00ffffff) {
+	} else if  (0x000c538b == *(uint32_t *) uc->uc_mcontext.gregs[REG_RIP] & 0x00ffffff) {
 		uc->uc_mcontext.gregs[REG_RDX] = 100000 + 3000 + counter;
 		uc->uc_mcontext.gregs[REG_RIP] += 3;
-	} else if  (0x0000558b == *(uint32_t *) uc->uc_mcontext.gregs[REG_RIP] && 0x00ffffff) {
+	} else if  (0x0000558b == *(uint32_t *) uc->uc_mcontext.gregs[REG_RIP] & 0x00ffffff) {
 		uc->uc_mcontext.gregs[REG_RDX] = 100000 + counter;
 		uc->uc_mcontext.gregs[REG_RIP] += 3;
-	} else if  (0x0004558b == *(uint32_t *) uc->uc_mcontext.gregs[REG_RIP] && 0x00ffffff) {
+	} else if  (0x0004558b == *(uint32_t *) uc->uc_mcontext.gregs[REG_RIP] & 0x00ffffff) {
 		uc->uc_mcontext.gregs[REG_RDX] = 100000 + 1000 + counter;
 		uc->uc_mcontext.gregs[REG_RIP] += 3;
-	} else if  (0x0008558b == *(uint32_t *) uc->uc_mcontext.gregs[REG_RIP] && 0x00ffffff) {
+	} else if  (0x0008558b == *(uint32_t *) uc->uc_mcontext.gregs[REG_RIP] & 0x00ffffff) {
 		uc->uc_mcontext.gregs[REG_RDX] = 100000 + 2000 + counter;
 		uc->uc_mcontext.gregs[REG_RIP] += 3;
-	} else if  (0x000c558b == *(uint32_t *) uc->uc_mcontext.gregs[REG_RIP] && 0x00ffffff) {
+	} else if  (0x000c558b == *(uint32_t *) uc->uc_mcontext.gregs[REG_RIP] & 0x00ffffff) {
 		uc->uc_mcontext.gregs[REG_RDX] = 100000 + 3000 + counter;
 		uc->uc_mcontext.gregs[REG_RIP] += 3;
 	} else if  (0x0b8b == *(uint16_t *) uc->uc_mcontext.gregs[REG_RIP]) {
 		uc->uc_mcontext.gregs[REG_RCX] = 100000 + counter;
 		uc->uc_mcontext.gregs[REG_RIP] += 2;
-	} else if  (0x00104d8b == *(uint32_t *) uc->uc_mcontext.gregs[REG_RIP] && 0x00ffffff) {
+	} else if  (0x00104d8b == *(uint32_t *) uc->uc_mcontext.gregs[REG_RIP] & 0x00ffffff) {
 		uc->uc_mcontext.gregs[REG_RCX] = 100000 + 4000 + counter;
 		uc->uc_mcontext.gregs[REG_RIP] += 3;
 	}
